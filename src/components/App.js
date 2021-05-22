c0b1142b873e1e3e2ff654f581612e8d47dc01e1
brunommbruno <brunommbruno@gmail.com>
2021-05-22 17:27:59 +0200

    changed app to class component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>asd</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
